551bc091c183440e8f09e6e51cc930ffc66dd289
Ndman90 <onwuamaegbund99@gmail.com>
2024-01-21 18:21:57 -0800

    Using Puppet to create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This puppet manifest creates a file at /tmp
+
+file { '/tmp/school':
+  ensure  => 'file',            # Ensure it is a regular file
+  mode    => '0744',            # Set file permissions
+  owner   => 'www-data',        # Set file owner
+  group   => 'www-data',        # Set file group
+  content => 'I love Puppet',   # Set file content
+}
