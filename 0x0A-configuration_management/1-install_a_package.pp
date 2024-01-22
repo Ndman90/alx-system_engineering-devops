b47d83aad53564dbeb8e719a7fb17d8900097865
Ndman90 <onwuamaegbund99@gmail.com>
2024-01-21 19:14:24 -0800

    Using Puppet to install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
 #This puppet manifest install flask from pip3
 
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+package { 'werkzeug':
+  ensure   => 'latest',
   provider => 'pip3',
 }
